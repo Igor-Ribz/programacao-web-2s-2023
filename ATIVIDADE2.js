7d7418d100f9314003fe8dcb2351e883ea9a2d76
Igor-Ribz <79918780+Igor-Ribz@users.noreply.github.com>
2023-08-16 10:39:01 -0300

    Atividade 2
    Atividade de javascript.

---
+++ ATIVIDADE2.js
@@ -0,0 +1,24 @@
+
+console.log("Parte 2 da atividade");
+var numero1 = 2; 
+var numero2 = 3;
+var numero3 = 4;
+
+var soma = numero1 + numero2 + numero3;
+var subtração = numero1 - numero2 - numero3;
+var divisão = numero3 / numero1;
+var multiplicação = numero1 * numero2 * numero3;
+
+
+console.log("soma", soma);
+console.log("subtração", subtração);
+console.log("divisão", divisão);
+console.log("multiplicação", multiplicação);
+
+console.log("///////////////////////////////////////////////");
+console.log("Parte 3 da atividade");
+console.log(`Resultado obtidio da soma é ${numero1+numero2+numero3}`);
+console.log(`Resultado obtidio da subtração é ${numero1-numero2-numero3}`);
+console.log(`Resultado obtidio da divisão é ${numero3/numero1}`);
+console.log(`Resultado obtidio da multiplicação é ${numero1*numero2*numero3}`);
+console.log("///////////////////////////////////////////////");
