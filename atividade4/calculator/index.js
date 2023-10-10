002032f3fb8c6f3ca27d4e7bc5e8d534f4a2ee0b
Igor-Ribz <79918780+Igor-Ribz@users.noreply.github.com>
2023-10-10 17:30:51 -0300

    Add files via upload

---
+++ atividade4/calculator/index.js
@@ -0,0 +1,45 @@
+// index.js
+const express = require('express');
+const app = express();
+
+
+const calculadora = require('./util/calculadora');
+
+app.get('/somar/:a/:b', (req, res) => {
+  const a = parseInt(req.params.a);
+  const b = parseInt(req.params.b);
+  const resultado = calculadora.somar(a, b);
+  res.send(`Resultado da soma: ${resultado}`);
+});
+
+
+app.get('/subtrair/:a/:b', (req, res) => {
+  const a = parseInt(req.params.a);
+  const b = parseInt(req.params.b);
+  const resultado = calculadora.subtrair(a, b);
+  res.send(`Resultado da subtração: ${resultado}`);
+});
+
+
+app.get('/multiplicar/:a/:b', (req, res) => {
+  const a = parseInt(req.params.a);
+  const b = parseInt(req.params.b);
+  const resultado = calculadora.multiplicar(a, b);
+  res.send(`Resultado da multiplicação: ${resultado}`);
+});
+
+app.get('/dividir/:a/:b', (req, res) => {
+  const a = parseInt(req.params.a);
+  const b = parseInt(req.params.b);
+  try {
+    const resultado = calculadora.dividir(a, b);
+    res.send(`Resultado da divisão: ${resultado}`);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
