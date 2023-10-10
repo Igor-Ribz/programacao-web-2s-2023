bcf8af3a66a3b1b734117473a28d8bcdea4aaaf3
Igor-Ribz <79918780+Igor-Ribz@users.noreply.github.com>
2023-10-10 15:06:37 -0300

    Exercicio1.js

---
+++ atividade2/exercicio1.js
@@ -0,0 +1,19 @@
+
+const numA = 10;
+const numB = 5;
+
+
+const soma = numA + numB;
+console.log(`Resultado da adição entre ${numA} e ${numB} = ${soma}`);
+
+
+const subtracao = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB} = ${subtracao}`);
+
+
+const multiplicacao = numA * numB;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB} = ${multiplicacao}`);
+
+
+const divisao = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB} = ${divisao}`);
